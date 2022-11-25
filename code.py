9db6c8b937ff6dcbb1893199f224a4cc54783d1f
kali <bilimdepaullilian@gmail.com>
2022-11-25 20:14:07 +0100

    sending up

--- code.py
+++ code.py
@@ -1,26 +1,34 @@
 def prime_number(limit):
 
-    prime_numbers = [1,2]
     count = 3
     test = 2
     prime = False
+
     if limit <= 2:
+
         print(1)
         print(2)
     else:
+
         while count < limit:
+
             while test < count:
+            
                 if count % test == 0:
-                    if count in prime_numbers:
-                        prime = False #not prime
+
+                    prime=False #not prime
                     count += 1
                     test=2
+
                 else:
+
                     prime = True #prime
                     test+=1
 
             if prime:
+
                 print(count)
+                
             count+=1
             test=2
             prime = False #putting back the default value of prime
